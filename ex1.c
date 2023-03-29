c5264ef2afff2439e7027df0e81b52117064bd4a
Jehyann <115532715+Jehyann@users.noreply.github.com>
2023-03-29 12:28:23 +0200

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,20 @@
+// Exercice 1 :
+// 1- Créer une structure Ville qui contient: - nom: string - nombre habitants: long
+// 2- Créer une liste de ville non classée par nombre d’habitants
+// 3- Créer une fonction classerVilles qui permet de trier la liste de ville dans l’ordre décroissant du
+// nombre d’habitant. (ex: tri bulle)
+// 4- Créer une fonction afficherClassement qui permet d’afficher le classement des villes.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct city {
+    char name[100];
+    long residents;
+    
+};
+
+void classifyCity (){
+    
+}
